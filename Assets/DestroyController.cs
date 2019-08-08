7f82c18fc07851580e0afa436ab257ad2d5428ee
YUYA <c.a.g.e.9.666@gmail.com>
2019-08-08 13:25:38 +0900

    Lesson6課題
    Lesson6課題途中、Unityちゃんの後ろに透明の壁を作成している段階。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyController : MonoBehaviour {
+
+	//Rendererコンポーネントを取得
+	private Renderer renderer;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter (Collision other){
+		Debug.Log (gameObject);
+		if(other.gameObject.tag == "Coin")
+		{   
+			Destroy(other.gameObject);
+		}
+	}
+}
