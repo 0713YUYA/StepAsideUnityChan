8f61d08c632a0b0bec6c37b303739b6fcd1bd077
YUYA <c.a.g.e.9.666@gmail.com>
2019-08-12 13:47:21 +0900

    Lesson6課題
    今、仕切りの壁をカメラの動きに合わせて動かして、その後に当たり判定のRigidbodyコンポーネントを追加させる際にUnityちゃんが再生の際に消えてしまう段階で止まっています。

--- Assets/DestroyController.cs
+++ Assets/DestroyController.cs
@@ -3,27 +3,29 @@ using System.Collections;
 
 public class DestroyController : MonoBehaviour {
 
-	//Rendererコンポーネントを取得
+	//Rendererコンポーネントを取得（課題）
 	private Renderer renderer;
-
+	//Unityちゃんと仕切り壁の距離（課題）
+	private float difference;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		
+	
 	}
-
-	void OnCollisionEnter (Collision other){
+	//仕切り壁とコインが当たった際に画面外のコインを破棄する（課題）
+	void OnCollisionEnter (Collision other)
+	{
 		Debug.Log (gameObject);
 		if(other.gameObject.tag == "Coin")
-		{   
-			Destroy(other.gameObject);
+		{
+			Destroy (other.gameObject);
 		}
-	}
-}
+	 }
+  }
